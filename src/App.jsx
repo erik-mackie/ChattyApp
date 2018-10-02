72db73725b6d77c22be09f2dffa49b973633b4ff
erik-mackie <erik.mackie@gmail.com>
2018-10-02 18:14:58 +0000

    Basic template provided loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React, {Component} from 'react';
+const dep = require
+import ChatBar from './ChatBar.jsx';
+import Message from './MessageList.jsx';
 
 class App extends Component {
   render() {
+
+
+
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <h1>Hello React :)</h1>
+        <ChatBar />
+        <Message />
+      </div>
     );
   }
 }
+
+
 export default App;
+
+
+{
+// responsible for storing all data for in this.state
+}
