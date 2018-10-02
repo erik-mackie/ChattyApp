e3887e1665188ac84d1ef04dabae8f46eecaed47
erik-mackie <erik.mackie@gmail.com>
2018-10-02 18:36:26 +0000

    faux loading

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,47 @@ const dep = require
 import ChatBar from './ChatBar.jsx';
 import Message from './MessageList.jsx';
 
+function Loading() {
+  return (
+    <h1>Loading...</h1>
+    )
+}
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading:true,
+      messages: [1,2,3,4],
+      currentUser: "Anonymous"
+    };
+  }
+
+  componentDidMount() {
+    // After 3 seconds, set `loading` to false in the state.
+    setTimeout(() => {
+      this.setState({loading: false}); // this triggers a re-render!
+    }, 1000)
+  }
   render() {
-
-
-
-    return (
-      <div>
-        <h1>Hello React :)</h1>
-        <ChatBar />
-        <Message />
-      </div>
-    );
+    //refactor as terary, this is gross, make use of less lines
+    if (this.state.loading) {
+      return (
+        <div>
+          <Loading />
+          <ChatBar />
+        </div>
+        )
+    } else {
+      return (
+        <div>
+          <h1>Hello React :)</h1>
+          <Message />
+          <ChatBar />
+        </div>
+      );
+    }
   }
 }
 
